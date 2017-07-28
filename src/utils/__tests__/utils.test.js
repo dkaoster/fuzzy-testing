b1649acafee3a181306e368a18bbfd307615ca0d
Daniel Kao <dkao@diplateevo.com>
2017-07-28 19:15:21 -0400

    add circle.yml (#2)
    * add circle.yml
    
    * add circle.yml
    
    * update tests
    
    * update tests
    
    * add token
    
    * update circle.yml
    
    * update tests
    
    * override
    
    * override
    
    * override

--- src/utils/__tests__/utils.test.js
+++ src/utils/__tests__/utils.test.js
@@ -1,11 +1,13 @@
+/* eslint-disable no-undef */
+
 import { combine, combineToArray, combineMultiple, combineMultipleLengths } from '../arrayUtils';
 
 describe('combine', () => {
   it('combines two arrays', () => {
     expect(combine(
-      [1, 2, 3], [4, 5, 6]
+      [1, 2, 3], [4, 5, 6],
     )).toEqual(
-      [[1, 4], [1, 5], [1, 6], [2, 4], [2, 5], [2, 6], [3, 4], [3, 5], [3, 6]]
+      [[1, 4], [1, 5], [1, 6], [2, 4], [2, 5], [2, 6], [3, 4], [3, 5], [3, 6]],
     );
   });
 });
@@ -13,9 +15,9 @@ describe('combine', () => {
 describe('combineToArray', () => {
   it('combines two arrays, one of which is a 2d array', () => {
     expect(combineToArray(
-      [[1], [2], [3]], [4, 5, 6]
+      [[1], [2], [3]], [4, 5, 6],
     )).toEqual(
-      [[1, 4], [1, 5], [1, 6], [2, 4], [2, 5], [2, 6], [3, 4], [3, 5], [3, 6]]
+      [[1, 4], [1, 5], [1, 6], [2, 4], [2, 5], [2, 6], [3, 4], [3, 5], [3, 6]],
     );
   });
 });
@@ -23,9 +25,9 @@ describe('combineToArray', () => {
 describe('combineMultiple', () => {
   it('combines one array multiple times', () => {
     expect(combineMultiple(
-      [1, 2], 3
+      [1, 2], 3,
     )).toEqual(
-      [[1, 1, 1], [1, 1, 2], [1, 2, 1], [1, 2, 2], [2, 1, 1], [2, 1, 2], [2, 2, 1], [2, 2, 2]]
+      [[1, 1, 1], [1, 1, 2], [1, 2, 1], [1, 2, 2], [2, 1, 1], [2, 1, 2], [2, 2, 1], [2, 2, 2]],
     );
   });
 });
@@ -33,19 +35,19 @@ describe('combineMultiple', () => {
 describe('combineMultipleLengths', () => {
   it('combines one array multiple times and saves all lengths', () => {
     expect(combineMultipleLengths(
-      [1, 2], 3
+      [1, 2], 3,
     )).toEqual(
       [[], [1], [2], [1, 1], [1, 2], [2, 1], [2, 2], [1, 1, 1], [1, 1, 2],
-        [1, 2, 1], [1, 2, 2], [2, 1, 1], [2, 1, 2], [2, 2, 1], [2, 2, 2]]
+        [1, 2, 1], [1, 2, 2], [2, 1, 1], [2, 1, 2], [2, 2, 1], [2, 2, 2]],
     );
   });
 
   it('combines one array multiple times and saves all lengths greater than or equal to 1', () => {
     expect(combineMultipleLengths(
-      [1, 2], 3, 2
+      [1, 2], 3, 2,
     )).toEqual(
       [[1, 1], [1, 2], [2, 1], [2, 2], [1, 1, 1], [1, 1, 2], [1, 2, 1],
-        [1, 2, 2], [2, 1, 1], [2, 1, 2], [2, 2, 1], [2, 2, 2]]
+        [1, 2, 2], [2, 1, 1], [2, 1, 2], [2, 2, 1], [2, 2, 2]],
     );
   });
 });
